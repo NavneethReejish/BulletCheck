7fc492683f517ed18fad2e6e01b1d2df7680a178
NavneethReejish <73111455+NavneethReejish@users.noreply.github.com>
2020-11-08 13:06:58 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+var bullet,wall,thickness;
+var speed,weight;
+
+
+
+function setup() {
+  createCanvas(1600,400);
+  speed=random(223,321);
+  weight=random(30,52);
+  thickness=random(22,83);
+  bullet = createSprite(50,200,50,5);
+  bullet.velocityX = speed;
+  bullet.shapeColor=color("white");
+
+  wall = createSprite(1200,200,thickness,height/2);
+  wall.shapeColor=color(255);
+}
+
+function draw() {
+  background(0); 
+if (bullet.collide(wall)) {
+   bullet.velocityX=0;
+   damage=0.5 * weight * speed* speed/(thickness *thickness *thickness);
+
+   if (damage>10) {
+      wall.shapeColor=("green");
+   }
+
+   if (damage<10) {
+      wall.shapeColor=("red");
+   }
+}
+  drawSprites();
+}
